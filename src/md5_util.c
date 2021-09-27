587e2a422f68594e83184e89aeab838499c4eb7e
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-09-27 12:03:18 +0200

    md5

--- src/md5_util.c
+++ src/md5_util.c
@@ -1,18 +1,22 @@
-#include <stdio.h>
-
 #include <openssl/md5.h>
 
 void md5(char *output, const char *input, size_t len) {
     MD5_CTX c;
-    unsigned char buf[MD5_DIGEST_LENGTH];
 
     MD5_Init(&c);
     MD5_Update(&c, input, len);
-    MD5_Final(buf, &c);
+    MD5_Final((unsigned char *)output, &c);
+}
 
-    char *p = output;
-    for (int i = 0; i < 16; i++) {
-        sprintf(p, "%02x", buf[i]);
-        p += 2;
+void raw2hex(char *hex, const char *raw, size_t len) {
+    static const char symbols[16] = {'0', '1', '2', '3', '4', '5', '6', '7',
+                                     '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+    for (int i = 0, j = 0; i < len; i++) {
+        int hi = (raw[i] >> 4) & 0xf;
+        int lo = raw[i] & 0xf;
+
+        hex[j++] = symbols[hi];
+        hex[j++] = symbols[lo];
     }
 }
--- src/md5_util.h
+++ src/md5_util.h
@@ -1,3 +1,4 @@
 #include <stdlib.h>
 
 void md5(char *output, char *input, size_t len);
+void raw2hex(char *hex, char *raw, size_t len);
