153da71a11b42a7418651de25a556aee69d120c0
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-10-15 10:39:27 +0200

    _

--- src/str_split_iter.c
+++ src/str_split_iter.c
@@ -7,6 +7,41 @@ typedef struct {
     bool get_len;
 } it_state;
 
+static bool it_next(void *_state, void *_dest) {
+    it_state *state = _state;
+    str_t *dest = _dest;
+
+    if (state->end) {
+        if (state->str == state->end) return false;
+    } else {
+        if (*state->str == (char)state->terminator) return false;
+    }
+
+    while (*state->str == state->delim) state->str++;
+    dest->start = state->str;
+
+    size_t _len = 0, *len = &_len;
+
+    if (state->get_len) {
+        len = &dest->len;
+    }
+
+    *len = 0;
+    if (state->end) {
+        while (*state->str != state->delim && state->str != state->end) {
+            state->str++;
+            (*len)++;
+        }
+    } else {
+        while (*state->str != state->delim &&
+               *state->str != state->terminator) {
+            state->str++;
+            (*len)++;
+        }
+    }
+    return true;
+}
+
 iterator str_split_iter(char *str, char delim, bool terminated,
                         size_t len_or_terminator, bool get_len) {
     iterator it;
@@ -29,30 +64,10 @@ iterator str_split_iter(char *str, char delim, bool terminated,
     return it;
 }
 
-static bool it_next(void *_state, void *_dest) {
-    it_state *state = _state;
-    str_t *dest = _dest;
-
-    while (*state->str == state->delim) state->str++;
-    dest->start = state->str;
-
-    size_t _len = 0, *len = &_len;
-
-    if (state->get_len) {
-        len = &dest->len;
-    }
-
-    *len = 0;
+void str_split_iter_refresh(iterator it, char *nstr, size_t nlen) {
+    it_state *state = it.state;
+    state->str = nstr;
     if (state->end) {
-        while (*state->str != state->delim && state->str != state->end) {
-            state->str++;
-            *len++;
-        }
-    } else {
-        while (*state->str != state->delim &&
-               *state->str != state->terminator) {
-            state->str++;
-            *len++;
-        }
+        state->end = nstr + nlen;
     }
 }
--- src/string_util.h
+++ src/string_util.h
@@ -7,6 +7,8 @@
 iterator str_split_iter(char *str, char delim, bool terminated,
                         size_t len_or_terminator, bool get_len);
 
+void str_split_iter_refresh(iterator it, char *nstr, size_t nlen);
+
 typedef struct {
     char *start;
     size_t len;
