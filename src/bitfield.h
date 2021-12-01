1aa43440ea5c5527eb6cd249aefa41c07e120baf
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-12-01 16:16:42 +0100

    _

--- src/bitfield.h
+++ src/bitfield.h
@@ -9,4 +9,4 @@ bool bitfield_get(uint64_t *bitfield, size_t index);
 size_t bitfield_count(uint64_t *bitfield, size_t amt_ints);
 static size_t bitfield_ints(size_t bits) { return 1 + (bits - 1) / 64; }
 uint64_t *bitfield_create(size_t bits);
-void *bitfield_free(uint64_t *bitfield);
+void bitfield_free(uint64_t *bitfield);
