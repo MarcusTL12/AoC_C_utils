3ee11df0479a841eb8d18fb19ced410e5273f77f
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-12-10 14:09:44 +0100

    Documented combinatorics

--- src/combinatorics.h
+++ src/combinatorics.h
@@ -3,5 +3,13 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+// Takes in the permutation state and updates it to the next one.
+// example for n = 5:
+// First state = {0, 0, 0, 0, 0}
+// Last state = {4, 3, 2, 1, 0}
+// returns false if the last state is given, true otherwise
 bool next_permstate(size_t *state, size_t n);
+
+// Takes the permutation state, genrerated by next_permstate,
+// and writes the permutation into *perm
 void get_perm(size_t *perm, size_t *state, size_t n);
