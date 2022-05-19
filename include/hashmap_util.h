69b43e2a2d19f1a761d8b0f702ae1a0eadc94ff0
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-05-19 15:25:31 +0200

    Added autogeneratable hash function and compare

---
+++ include/hashmap_util.h
@@ -0,0 +1,8 @@
+
+#define MAKE_HASH_AND_CMP(NAME, SIZE) \
+static uint64_t NAME##_hash(const void *item, uint64_t s0, uint64_t s1) { \
+    return hashmap_sip(item, SIZE, s0, s1); \
+} \
+static int NAME##_cmp(const void *a, const void *b, void *udata) { \
+    return memcmp(a, b, SIZE); \
+}
