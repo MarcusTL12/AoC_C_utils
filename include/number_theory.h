a871353ffee2d38730bb90c4c7f35ea3a7fcf64e
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-05-22 01:17:35 +0200

    Modular arithmetic

--- include/number_theory.h
+++ include/number_theory.h
@@ -3,8 +3,21 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#define signum(x) x >= 0 ? x > 0 ? 1 : 0 : -1
+
 static int64_t gcd(int64_t a, int64_t b) {
-    return b != 0 ? gcd(b, a % b) : labs(a);
+    return b != 0 ? gcd(b, a % b) : llabs(a);
 }
 
 static int64_t lcm(int64_t a, int64_t b) { return a * b / gcd(a, b); }
+
+typedef struct {
+    int64_t g, x, y;
+} gcdx_t;
+
+gcdx_t gcdx(int64_t a, int64_t b);
+
+static int64_t invmod(int64_t a, int64_t b) { return gcdx(a, b).x; }
+
+int64_t modmul(int64_t a, int64_t b, int64_t n);
+int64_t modexp(int64_t a, int64_t b, int64_t n);
--- src/number_theory.c
+++ src/number_theory.c
@@ -1,1 +1,40 @@
 #include "number_theory.h"
+
+gcdx_t gcdx(int64_t a, int64_t b) {
+    if (!b) {
+        return (gcdx_t){llabs(a), signum(a), 0};
+    }
+
+    lldiv_t ld = lldiv(a, b);
+
+    gcdx_t g = gcdx(b, ld.rem);
+    return (gcdx_t){g.g, g.y, g.x - g.y * ld.quot};
+}
+
+int64_t modmul(int64_t a, int64_t b, int64_t n) {
+    int64_t acc = 0;
+
+    if (b < 0) {
+        b += n;
+    }
+
+    while (b) {
+        acc = (acc + (b & 1 ? a : 0)) % n;
+        a = (a + a) % n;
+        b >>= 1;
+    }
+
+    return acc;
+}
+
+int64_t modexp(int64_t a, int64_t b, int64_t n) {
+    int64_t acc = 1;
+
+    while (b) {
+        acc = modmul(acc, (b & 1 ? a : 1), n);
+        a = modmul(a, a, n);
+        b >>= 1;
+    }
+
+    return acc;
+}
