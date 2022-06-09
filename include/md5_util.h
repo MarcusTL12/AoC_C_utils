30176c18ddeee09b867352428387e41b40e14e02
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-06-10 00:52:47 +0200

    Added docs

--- include/md5_util.h
+++ include/md5_util.h
@@ -2,5 +2,9 @@
 
 #include <stdlib.h>
 
+// output: pointer to 16-byte buffer
 void md5(char *output, char *input, size_t len);
+
+// hex: output pointer to 2 * len byte buffer
+// raw: input pointer to len byte buffer
 void raw2hex(char *hex, char *raw, size_t len);
